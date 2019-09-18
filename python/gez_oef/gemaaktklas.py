bc161c260b8decead660fc6dc0b495bf1b95650c
Tom Vanhoutte <syntraprogrammeurs@gmail.com>
2019-09-18 10:34:31 +0200

    einde selecties

--- python/gez_oef/gemaaktklas.py
+++ python/gez_oef/gemaaktklas.py
@@ -1,11 +1,51 @@
 #wijziging
-ab
-class d
-fd(object):
-	"""docstring for d
-	fd"""
-	def __init__(self, arg):
-		super(d
-			fd, self).__init__()
-		self.arg = arg
-		
+
+
+# datatypes
+#-----------
+# int = integer = getal = nummer
+# string = tekst
+# boolean = (true or false) (1 of 0)
+
+#functies (built in functies van python zelf)
+#----------------------------------------------
+# herkenning van een functie (ronde haakjes)
+
+# int() = functie zonder parameter
+# int(parameter) = functie met parameter
+# int() => die zorgt voor omzetting van tekst naar getal
+
+# input()=> input vraagt aan de gebruiker
+# input("Geef een getal in") => vraagt input aan de gebruiker
+
+# print()=> tekst op het scherm weergeven
+
+# controlestructuren
+#---------------------
+# SEQUENTIE
+# is één enkele lijn in code
+
+# SELECTIE
+# ENKELVOUDIGE SELECTIE
+# if (conditie):
+# else:
+
+# TWEEVOUDIGE SELECTIE
+# if (conditie):
+# elif (conditie):
+# else:
+
+# MEERVOUDIGE SELECTIE
+# if (conditie):
+# elif (conditie):
+# elif (conditie):
+# elif (conditie):
+# else:
+
+# GENESTE SELECTIE
+# if (conditie):
+	# if (conditie):
+	# elif (conditie):
+	# else:
+# else:
+
