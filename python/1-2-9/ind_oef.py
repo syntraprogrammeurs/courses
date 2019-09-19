41c54db7345851ed0634376e455d56b955d67e49
Tom Vanhoutte <syntraprogrammeurs@gmail.com>
2019-09-19 15:36:16 +0200

    priem

--- python/1-2-9/ind_oef.py
+++ python/1-2-9/ind_oef.py
@@ -1,13 +1,16 @@
 #priemgetallen
 
-getal = int(input("Geef een getal in groter dan nul:"))
-afdrukOpscherm=""
-teller= 0
-
-for x in range(1, getal):
-	if(x%2 != 0):
-		teller = teller + 1
-		if(teller <=2):
-			afdrukOpscherm = afdrukOpscherm + str(x) + ","
-	teller = 0
+afdrukOpscherm =""
+getal = int(input("geef een eindgetal in:"))
+ 
+for x in range(2, getal):
+    for y in range(2, x):
+        if x % y == 0:
+            print(x,'=',y,'*',x/y)
+            break
+    else:
+        print(x,'is een priemgetal')
+        afdrukOpscherm = afdrukOpscherm + str(x) + ","
+ 
 print(afdrukOpscherm[:-1])
+	
